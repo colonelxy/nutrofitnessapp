a485371f04045e6db4f924abac8c57cc9a166d8b
colonelxy <harold2@protonmail.com>
2022-06-14 08:24:04 +0300

    Added button to HeroBanner

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-import { Box, Stack, Typography } from '@mui/material';
+import { Box, Stack, Typography, Button } from '@mui/material';
 
 const HeroBanner = () => {
   return (
@@ -9,15 +9,20 @@ const HeroBanner = () => {
       mt: { lg: '212px', xs: '70px'}, 
       ml: { sm: '50px'}
     }} position= 'relative' p='20px'>
-      <Typography color="#FF2625" fontweight="600" fontSize="26px">
+      <Typography color="#FF2625" fontWeight="600" fontSize="26px">
         Nutrofitness
       </Typography>
-      <Typography>
+      <Typography fontWeight= {700}
+      sx={{fontSize: { lg: '44px', xs: '40px'}}}>
         Sweat, Smile <br /> and Repeat
       </Typography>
-      <Typography>
+      <Typography fontSize='22px'
+      lineHeight= '35px'>
         Check out the most effective exercises
       </Typography>
+      <Button>
+        Explore Exercises
+      </Button>
     </Box>
   )
 }
