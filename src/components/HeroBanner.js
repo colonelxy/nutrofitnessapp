7ec8c49696a1388aa7d0f17789348462037db861
colonelxy <harold2@protonmail.com>
2022-06-14 08:15:59 +0300

    Style and typography in HeroBanner

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
+
 import { Box, Stack, Typography } from '@mui/material';
 
 const HeroBanner = () => {
   return (
-    <div>HeroBanner</div>
+    <Box sx={{
+      mt: { lg: '212px', xs: '70px'}, 
+      ml: { sm: '50px'}
+    }} position= 'relative' p='20px'>
+      <Typography color="#FF2625" fontweight="600" fontSize="26px">
+        Nutrofitness
+      </Typography>
+      <Typography>
+        Sweat, Smile <br /> and Repeat
+      </Typography>
+      <Typography>
+        Check out the most effective exercises
+      </Typography>
+    </Box>
   )
 }
 
